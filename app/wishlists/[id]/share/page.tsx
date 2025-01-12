f73ce03ebd83c35f09adb04f7e8c8f252292926c
malochak <konrad_malocha@hotmail.com>
2025-01-12 14:01:20 +0100

    add dummy share page

---
+++ app/wishlists/[id]/share/page.tsx
@@ -0,0 +1,13 @@
+// Public wishlist view
+export default async function PublicWishlistPage({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  // Implementation here
+
+  // centered plaveholder to be implemented
+  return <div className="flex justify-center mt-20 h-screen">
+    <p className="text-2xl font-bold">To be implemented</p>
+  </div>;
+} 
